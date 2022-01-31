341758b36d44e41f5b21c350e23ec99b7f615c97
pratyusha05 <pratyushap05@gmail.com>
2022-01-31 11:47:27 +0530

    added firebase.js

---
+++ slack/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD3NPjPvyjNQsFRnQeGm3JxRH-J4_ds21Y",
+    authDomain: "slackclone-94ed8.firebaseapp.com",
+    projectId: "slackclone-94ed8",
+    storageBucket: "slackclone-94ed8.appspot.com",
+    messagingSenderId: "172102310378",
+    appId: "1:172102310378:web:050a14d5a9cce84f81fa93",
+    measurementId: "G-Y7J8HPKJWE"
+  };

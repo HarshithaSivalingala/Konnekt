5b6e0ce4b3e549348fbf0afc0dfc7f9524fa0da0
sritulasi <19wh1a0488@bvrithyderabad.edu.in>
2022-01-31 14:14:48 +0530

    Added React Router part

--- slack/src/App.js
+++ slack/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Slack app</h1>
+      
+      <Router>
+      <>
+        <Switch>
+          <Route path="/" exact >
+            <Header/>
+          </Route>
+        </Switch>
+      </>
+    </Router>
     </div>
   );
 }
--- slack/src/index.js
+++ slack/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+
   <React.StrictMode>
     <Provider store={store}>
       <App />

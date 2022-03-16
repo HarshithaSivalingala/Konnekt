675896d1622880baf550c8b934042d8d7be2aae3
harshitha_022 <19wh1a1253@bvrithyderabad.edu.in>
2022-03-16 14:31:24 +0530

    edited sidebar

--- slack/src/components/SideBar/Channels.component.jsx
+++ slack/src/components/SideBar/Channels.component.jsx
@@ -1,307 +1,320 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import firebase from "firebase";
-import { setChannel} from "../../store/actioncreator";
-import { Notification } from "./Notification.component";
-import onSubmit1 from "./Private.component";
-import './Channels.css';
-import { Menu, Icon, Modal, Button, Form, Segment} from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import firebase from 'firebase'
+import { setChannel } from '../../store/actioncreator'
+import { Notification } from './Notification.component'
+import onSubmit1 from './Private.component'
+import './Channels.css'
+import { Menu, Icon, Modal, Button, Form, Segment } from 'semantic-ui-react'
+import { Public } from '@material-ui/icons'
 
 const Channels = (props) => {
-    const [modalOpenState, setModalOpenState] = useState(false);
-    const [modalOpenState1, setModalOpenState1] = useState(false);
-    const [modalopen1, setModalOpen1] = useState(false);
-    const [modalopen, setModalOpen] = useState(false);
-    const [channelAddState, setChannelAddState] = useState({ name: '', description: '', password: ''});
-    const [isLoadingState, setLoadingState] = useState(false);
-    const [pubchannelsState, setpubChannelsState] = useState([]);
-    const [privateOpen,privateSet] = useState(false);
-    const [prichannelsState, setpriChannelsState] = useState([]);
+  const [modalOpenState, setModalOpenState] = useState(false)
+  const [modalOpenState1, setModalOpenState1] = useState(false)
+  const [modalopen1, setModalOpen1] = useState(false)
+  const [modalopen, setModalOpen] = useState(false)
+  const [channelAddState, setChannelAddState] = useState({
+    name: '',
+    description: '',
+    password: '',
+  })
+  const [isLoadingState, setLoadingState] = useState(false)
+  const [pubchannelsState, setpubChannelsState] = useState([])
+  const [privateOpen, privateSet] = useState(false)
+  const [prichannelsState, setpriChannelsState] = useState([])
 
-    
+  const channelsRef = firebase.database().ref('pubchannels')
+  const usersRef = firebase.database().ref('users')
 
-    const channelsRef = firebase.database().ref("pubchannels");
-    const usersRef = firebase.database().ref("users");
+  const channels = firebase.database().ref('prichannels')
+  const users = firebase.database().ref('users')
 
-    const channels = firebase.database().ref("prichannels");
-    const users = firebase.database().ref("users");
+  useEffect(() => {
+    channelsRef.on('child_added', (snap) => {
+      setpubChannelsState((currentState) => {
+        let updatedState = [...currentState]
+        updatedState.push(snap.val())
+        return updatedState
+      })
+    })
 
+    return () => channelsRef.off()
+  }, [])
 
-    useEffect(() => {
-        channelsRef.on('child_added', (snap) => {
-            setpubChannelsState((currentState) => {
-                let updatedState = [...currentState];
-                updatedState.push(snap.val());               
-                return updatedState;
-            })
-        });
-
-        return () => channelsRef.off();
-    }, [])
-
-    useEffect(()=> {
-        if (pubchannelsState.length > 0) {
-            props.selectChannel(pubchannelsState[0])
-        }
-    },[!props.channel ?pubchannelsState : null ])
-
-    useEffect(() => {
-        channels.on('child_added', (snapshot) => {
-            setpriChannelsState((currentState) => {
-                let updatedState = [...currentState];
-                updatedState.push(snapshot.val());               
-                return updatedState;
-            })
-        });
-
-        return () => channels.off();
-    }, [])
-
-    useEffect(()=> {
-        if (prichannelsState.length > 0) {
-            props.selectChannel(prichannelsState[0])
-        }
-    },[!props.channel ?prichannelsState : null ])
-
-    const openModalPrivate = () => {
-        setModalOpenState1(true);
+  useEffect(() => {
+    if (pubchannelsState.length > 0) {
+      props.selectChannel(pubchannelsState[0])
     }
+  }, [!props.channel ? pubchannelsState : null])
 
-    const closeModalPrivate = () => {
-        setModalOpenState1(false);
-          
+  useEffect(() => {
+    channels.on('child_added', (snapshot) => {
+      setpriChannelsState((currentState) => {
+        let updatedState = [...currentState]
+        updatedState.push(snapshot.val())
+        return updatedState
+      })
+    })
+
+    return () => channels.off()
+  }, [])
+
+  useEffect(() => {
+    if (prichannelsState.length > 0) {
+      props.selectChannel(prichannelsState[0])
     }
+  }, [!props.channel ? prichannelsState : null])
 
-    const openModal = () => {
-        setModalOpenState(true);
-    }
-    const openModal2 = () => {
-        setModalOpen(true);
-    }
+  const openModalPrivate = () => {
+    setModalOpenState1(true)
+  }
 
-    const closeModal = () => {
-        setModalOpenState(false);
-    }
-    const closeModal2 = () => {
-        setModalOpen(false);
-    }
+  const closeModalPrivate = () => {
+    setModalOpenState1(false)
+  }
 
-    const checkIfFormValid = () => {
-        return channelAddState && channelAddState.name && channelAddState.description && channelAddState.password;
-    }
+  const openModal = () => {
+    setModalOpenState(true)
+  }
+  const openModal2 = () => {
+    setModalOpen(true)
+  }
 
-    const displayChannels = () => {
-        if (pubchannelsState.length > 0) {
-            return pubchannelsState.map((channel) => {
-                return <Menu.Item
-                    key={channel.id}
-                    name={channel.name}
-                    onClick={() => selectChannel(channel)}
-                    active={props.channel && channel.id === props.channel.id && !props.channel.isFavourite}
-                >
-                      <Notification user={props.user} channel={props.channel}
-                        notificationChannelId={channel.id}
-                        displayName= {"# " + channel.name} />
-                   
-                </Menu.Item>
-            })
-        }
- 
-        }
-    
+  const closeModal = () => {
+    setModalOpenState(false)
+  }
+  const closeModal2 = () => {
+    setModalOpen(false)
+  }
 
-    const selectChannel = (channel) => {
-        setLastVisited(props.user,props.channel);
-        setLastVisited(props.user,channel);
-        props.selectChannel(channel);
-    }
+  const checkIfFormValid = () => {
+    return (
+      channelAddState &&
+      channelAddState.name &&
+      channelAddState.description &&
+      channelAddState.password
+    )
+  }
 
-    const setLastVisited = (user, channel) => {
-        const lastVisited = usersRef.child(user.uid).child("lastVisited").child(channel.id);
-        lastVisited.set(firebase.database.ServerValue.TIMESTAMP);
-        lastVisited.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
-    }
-
-   
- 
-
-    const onSubmit = () => {
-        setModalOpen(false);
-        privateSet(false);
-        
-       
-
-        if (!checkIfFormValid()) {
-            return false;
-        }
-
-        const key = channelsRef.push().key;
-
-        const channel = {
-            id: key,
-            name: channelAddState.name,
-            description: channelAddState.description,
-            created_by: {
-                name: props.user.displayName,
-                avatar: props.user.photoURL
+  const displayChannels = () => {
+    if (pubchannelsState.length > 0) {
+      return pubchannelsState.map((channel) => {
+        return (
+          <Menu.Item
+            key={channel.id}
+            name={channel.name}
+            onClick={() => selectChannel(channel)}
+            active={
+              props.channel &&
+              channel.id === props.channel.id &&
+              !props.channel.isFavourite
             }
-        }
-        setLoadingState(true);
-        channelsRef.child(key)
-            .update(channel)
-            .then(() => {
-                setChannelAddState({ name: '', description: '' });
-                setLoadingState(false);
-                closeModal();
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+          >
+            <Notification
+              user={props.user}
+              channel={props.channel}
+              notificationChannelId={channel.id}
+              displayName={'# ' + channel.name}
+            />
+          </Menu.Item>
+        )
+      })
+    }
+  }
+
+  const selectChannel = (channel) => {
+    setLastVisited(props.user, props.channel)
+    setLastVisited(props.user, channel)
+    props.selectChannel(channel)
+  }
+
+  const setLastVisited = (user, channel) => {
+    const lastVisited = usersRef
+      .child(user.uid)
+      .child('lastVisited')
+      .child(channel.id)
+    lastVisited.set(firebase.database.ServerValue.TIMESTAMP)
+    lastVisited.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP)
+  }
+
+  const onSubmit = () => {
+    setModalOpen(false)
+    privateSet(false)
+
+    if (!checkIfFormValid()) {
+      return false
     }
 
-    
+    const key = channelsRef.push().key
 
-    const handleInput = (e) => {
-
-        let target = e.target;
-        setChannelAddState((currentState) => {
-            let updatedState = { ...currentState };
-            updatedState[target.name] = target.value;
-            return updatedState;
-        })
+    const channel = {
+      id: key,
+      name: channelAddState.name,
+      description: channelAddState.description,
+      created_by: {
+        name: props.user.displayName,
+        avatar: props.user.photoURL,
+      },
     }
+    setLoadingState(true)
+    channelsRef
+      .child(key)
+      .update(channel)
+      .then(() => {
+        setChannelAddState({ name: '', description: '' })
+        setLoadingState(false)
+        closeModal()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
-    return <> <Menu.Menu style={{ marginTop: '35px' }}>
+  const handleInput = (e) => {
+    let target = e.target
+    setChannelAddState((currentState) => {
+      let updatedState = { ...currentState }
+      updatedState[target.name] = target.value
+      return updatedState
+    })
+  }
+
+  return (
+    <>
+      {' '}
+      <Menu.Menu style={{ marginTop: '10px' }}>
         <Menu.Item>
-            <span className="clickable" onClick={openModal2} >
-                <p><Icon name="add" />ADD CHANNEL</p>
-            </span>
+          <span className='clickable mains' onClick={openModal2}>
+            <Icon name='add' /> ADD CHANNEL
+          </span>
         </Menu.Item>
-        <selectChannel1/>
-        <br></br>  
-        <Menu.Item style={{fontSize : '17px'}}>
-            <span className="clickable">
-                <p><Icon name="bullhorn"/>Public</p>
-            </span>
-            <tb/>({pubchannelsState.length})
+        <selectChannel1 />
+
+        <Menu.Item style={{ fontSize: '17px' }}>
+          <span className='mains'>
+            <Public fontSize='large' />
+            <span> Public({pubchannelsState.length})</span>{' '}
+          </span>
         </Menu.Item>
         {displayChannels()}
         <Menu.Item>
-        <Modal size={'tiny'}open={modalopen} onClose={closeModal}>
-            <Modal.Header>
-                Choose a new Workspace 
-            </Modal.Header>
-            
+          <Modal size={'tiny'} open={modalopen} onClose={closeModal}>
+            <Modal.Header>Choose a new Workspace</Modal.Header>
+
             <Modal.Actions>
-                <Button style={{float: 'left'}} loading={isLoadingState} onClick={openModal}>
-                    <Icon name="bullhorn" /> Public 
-                </Button>
-                <Button loading={isLoadingState} onClick={closeModal2}>
-                    Close
-                </Button>
-                <Button style={{float: 'left'}} loading={isLoadingState} onClick={openModalPrivate}>
-                        <Icon name="user secret" /> Private  
-                </Button>
+              <Button
+                style={{ float: 'left' }}
+                loading={isLoadingState}
+                onClick={openModal}
+              >
+                <Icon name='bullhorn' /> Public
+              </Button>
+              <Button loading={isLoadingState} onClick={closeModal2}>
+                Close
+              </Button>
+
+              <Button
+                style={{ float: 'left' }}
+                loading={isLoadingState}
+                onClick={openModalPrivate}
+              >
+                <Icon name='user secret' /> Private
+              </Button>
             </Modal.Actions>
-        </Modal>
+          </Modal>
         </Menu.Item>
-    </Menu.Menu>
-    
-    <Modal open={modalOpenState1} onClose={closeModalPrivate}>
-            <Modal.Header>
-                Create Private Channel
-            </Modal.Header>
-            <Modal.Content>
-                <Form onSubmit={onSubmit}>
-                    <Segment stacked>
-                        <Form.Input
-                            name="name"
-                            value={channelAddState.name}
-                            onChange={handleInput}
-                            type="text"
-                            placeholder="Enter Channel Name"
-                        />
-                        <Form.Input
-                            name="description"
-                            value={channelAddState.description}
-                            onChange={handleInput}
-                            type="text"
-                            placeholder="User ID"
-                        />
-                        <Form.Input
-                            name="password"
-                            value={channelAddState.password}
-                            onChange={handleInput}
-                            type="password"
-                            placeholder="Room Code"
-                        />
-                        <Form.Input
-                            name="confirmpassword"
-                            value={channelAddState.confirmpassword}
-                            onChange={handleInput}
-                            type="password"
-                            placeholder="Confirm Room Code"
-                        />
-                    </Segment>
-                </Form>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button loading={isLoadingState} onClick={onSubmit1}>
-                    <Icon name="checkmark" /> Save
-                </Button>
-                <Button onClick={closeModalPrivate}>
-                    <Icon name="remove" /> Cancel
-                </Button>
-            </Modal.Actions>
-        </Modal>
-        
-        <Modal open={modalOpenState} onClose={closeModal}>
-            <Modal.Header>
-                Create Channel
-            </Modal.Header>
-            <Modal.Content>
-                <Form onSubmit={onSubmit}>
-                    <Segment stacked>
-                        <Form.Input
-                            name="name"
-                            value={channelAddState.name}
-                            onChange={handleInput}
-                            type="text"
-                            placeholder="Enter Channel Name"
-                        />
-                        <Form.Input
-                            name="description"
-                            value={channelAddState.description}
-                            onChange={handleInput}
-                            type="text"
-                            placeholder="Enter Channel Description"
-                        />
-                    </Segment>
-                </Form>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button loading={isLoadingState} onClick={onSubmit}>
-                    <Icon name="checkmark" /> Save
-                </Button>
-                <Button onClick={closeModal}>
-                    <Icon name="remove" /> Cancel
-                </Button>
-            </Modal.Actions>
-        </Modal>
+      </Menu.Menu>
+      <Modal open={modalOpenState1} onClose={closeModalPrivate}>
+        <Modal.Header>Create Private Channel</Modal.Header>
+        <Modal.Content>
+          <Form onSubmit={onSubmit}>
+            <Segment stacked>
+              <Form.Input
+                name='name'
+                value={channelAddState.name}
+                onChange={handleInput}
+                type='text'
+                placeholder='Enter Channel Name'
+              />
+              <Form.Input
+                name='description'
+                value={channelAddState.description}
+                onChange={handleInput}
+                type='text'
+                placeholder='User ID'
+              />
+              <Form.Input
+                name='password'
+                value={channelAddState.password}
+                onChange={handleInput}
+                type='password'
+                placeholder='Room Code'
+              />
+              <Form.Input
+                name='confirmpassword'
+                value={channelAddState.confirmpassword}
+                onChange={handleInput}
+                type='password'
+                placeholder='Confirm Room Code'
+              />
+            </Segment>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button loading={isLoadingState} onClick={onSubmit1}>
+            <Icon name='checkmark' /> Save
+          </Button>
+          <Button onClick={closeModalPrivate}>
+            <Icon name='remove' /> Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal>
+      <Modal open={modalOpenState} onClose={closeModal}>
+        <Modal.Header>Create Channel</Modal.Header>
+        <Modal.Content>
+          <Form onSubmit={onSubmit}>
+            <Segment stacked>
+              <Form.Input
+                name='name'
+                value={channelAddState.name}
+                onChange={handleInput}
+                type='text'
+                placeholder='Enter Channel Name'
+              />
+              <Form.Input
+                name='description'
+                value={channelAddState.description}
+                onChange={handleInput}
+                type='text'
+                placeholder='Enter Channel Description'
+              />
+            </Segment>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button loading={isLoadingState} onClick={onSubmit}>
+            <Icon name='checkmark' /> Save
+          </Button>
+          <Button onClick={closeModal}>
+            <Icon name='remove' /> Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </>
+  )
 }
 
 const mapStateToProps = (state) => {
-    return {
-        user: state.user.currentUser,
-        channel: state.channel.currentChannel
-    }
+  return {
+    user: state.user.currentUser,
+    channel: state.channel.currentChannel,
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        selectChannel: (channel) => dispatch(setChannel(channel))
-    }
+  return {
+    selectChannel: (channel) => dispatch(setChannel(channel)),
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Channels);
+export default connect(mapStateToProps, mapDispatchToProps)(Channels)
--- slack/src/components/SideBar/FavouriteChannels.component.jsx
+++ slack/src/components/SideBar/FavouriteChannels.component.jsx
@@ -1,48 +1,62 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { setChannel } from "../../store/actioncreator"
+import React from 'react'
+import { connect } from 'react-redux'
+import { setChannel } from '../../store/actioncreator'
 
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon } from 'semantic-ui-react'
 
 const FavouriteChannels = (props) => {
+  const displayChannels = () => {
+    if (Object.keys(props.favouriteChannels).length > 0) {
+      return Object.keys(props.favouriteChannels).map((channelId) => {
+        return (
+          <Menu.Item
+            key={channelId}
+            name={props.favouriteChannels[channelId]}
+            onClick={() =>
+              props.selectChannel({
+                id: channelId,
+                name: props.favouriteChannels[channelId],
+                isFavourite: true,
+              })
+            }
+            active={
+              props.channel &&
+              channelId === props.channel.id &&
+              props.channel.isFavourite
+            }
+          >
+            {'# ' + props.favouriteChannels[channelId]}
+          </Menu.Item>
+        )
+      })
+    }
+  }
 
-    const displayChannels = () => {
-        if (Object.keys(props.favouriteChannels).length > 0) {
-            return Object.keys(props.favouriteChannels).map((channelId) => {
-                return <Menu.Item
-                    key={channelId}
-                    name={props.favouriteChannels[channelId]}
-                    onClick={() => props.selectChannel({ id: channelId, name: props.favouriteChannels[channelId] , isFavourite : true })}
-                    active={props.channel && channelId === props.channel.id && props.channel.isFavourite}
-                >
-                    {"# " + props.favouriteChannels[channelId]}
-                </Menu.Item>
-            })
-        }
-    } 
+  return (
+    <Menu.Menu>
+      <Menu.Item style={{ fontSize: '17px' }}>
+        <span className='mains'>
+          <Icon name='star' />
 
-    return <Menu.Menu >
-        <Menu.Item style={{ fontSize: '17px' }}>
-            <span>
-                <Icon name="star" /> Starred
-            </span>
-            ({Object.keys(props.favouriteChannels).length})
-        </Menu.Item>
-        {displayChannels()}
+          <span>Starred ({Object.keys(props.favouriteChannels).length})</span>
+        </span>
+      </Menu.Item>
+      {displayChannels()}
     </Menu.Menu>
+  )
 }
 
 const mapStateToProps = (state) => {
-    return {
-        channel: state.channel.currentChannel,
-        favouriteChannels: state.favouriteChannel.favouriteChannel
-    }
+  return {
+    channel: state.channel.currentChannel,
+    favouriteChannels: state.favouriteChannel.favouriteChannel,
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        selectChannel: (channel) => dispatch(setChannel(channel))
-    }
+  return {
+    selectChannel: (channel) => dispatch(setChannel(channel)),
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(FavouriteChannels);
+export default connect(mapStateToProps, mapDispatchToProps)(FavouriteChannels)
